e871114a8c62871d2571b8bd4bda6c31844306e3
HelloThereGeneralKenobi <37271141+HelloThereGeneralKenobi@users.noreply.github.com>
2021-01-17 17:57:59 -0800

    Finished LambdaButtons

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,11 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -20,6 +22,23 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
+		
+		addNumbers.addActionListener((e)->{
+			String answer1 = JOptionPane.showInputDialog("Add a number");
+			String answer2 = JOptionPane.showInputDialog("Add another number");
+			int sum = Integer.parseInt(answer1) + Integer.parseInt(answer2);
+			System.out.println(sum);
+		});
+		
+		randNumber.addActionListener((e)->{
+			Random randGen = new Random();
+			System.out.println(randGen.nextInt(100));
+		});
+		
+		
+		tellAJoke.addActionListener((e)->{
+			System.out.println("Whats up with airplane food?");
+		});
 		
 		window.setVisible(true);
 		window.pack();
